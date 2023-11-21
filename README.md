d5f0d7f2a7fbadb30527e19e42e23ff7a58b30d8
weaamA19 <weaam.ajoor@gmail.com>
2023-11-21 21:47:13 +0300

    Initial JavaScript and Some other updates

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Phonic-Fun
 
+---
+
 ## Data Collected
 
 The data was obtained from the folloiwng source:
@@ -7,3 +9,9 @@ https://www.jollylearning.co.uk/resource-bank/learn-the-letter-sounds/
 
 Sound Icon:
 https://iconscout.com/free-icon/sound-2451537
+
+Useful Resources:
+1-Object keys, values, and entries methods: https://youtu.be/VmicKaGcs5g?si=_aCRqlbM_XVwy6g6
+2-Object keys: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+3-Randomise Elements:https://www.geeksforgeeks.org/how-to-select-a-random-element-from-array-in-javascript/
+4-Bootstrap: https://getbootstrap.com/docs/5.3/getting-started/introduction/
---
+++ js/game.js
@@ -0,0 +1,201 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+
+
+// Breakdown javaScript
+
+// Set the Initial Variables
+let round = 1; //round number
+let previousPhonic=[];
+let selectedPhonic;
+let scoreA;
+let scoreB;
+let currentPlayer = "A"; // Starting player
+
+    //About the Sounds:
+    //listen to the user click
+    const soundGenerator = document.getElementById("generateSound");
+    soundGenerator.addEventListener("click", phonicGenerator);
+
+    function phonicGenerator(){
+
+        //Get the dataset of the current round
+            let datasetKey = "round"+round;
+            let dataSet = phonicData[datasetKey];
+            // console.log(Object.keys(dataSet.randomSound));
+        
+        //Select Randomly the letters sounds (ex."round1" ->"randomSound" --> "phonic1")
+            let obtainKeys= Object.keys(dataSet.randomSound); 
+            console.log(obtainKeys);
+
+        //To avoid selecting previous Phonic a while loop was used 
+        //add condition to ensure after 6 sounds played it alters "Round Ended"
+            if (previousPhonic.length < 6){
+                generateRandomPhonic(obtainKeys); }
+
+            else {
+                alert("The Round has Ended"); 
+                return;
+            }
+        
+            console.log(selectedPhonic);
+            console.log(previousPhonic);
+
+        // Access the the audio element
+        let  soundName = Object.values(dataSet.randomSound[selectedPhonic]);
+        console.log(soundName);
+
+        //Define the Sound Path
+        let soundPath= './sounds/round' + round + '/' + soundName[0];
+        console.log(soundPath);
+
+        // Get the audio element
+        const audioElement = document.getElementById('audioContainer');
+
+        // Set attributes and properties
+
+        audioElement.src = soundPath; // Set the audio source
+        
+        audioElement.controls = true;  // Enable the audio controls (play)
+        
+        // Play the audio
+        audioElement.play();
+    }
+
+
+    function generateRandomPhonic(obtainKeys) {
+        while (true){
+            //Generate random phonic
+            selectedPhonic = obtainKeys[Math.floor(Math.random() * obtainKeys.length)];
+            
+            //Check if the element is not previously selected
+            if(!previousPhonic.includes(selectedPhonic)) 
+            {
+                //Store the current selected phonic in "previousPhonic" array
+                previousPhonic.push(selectedPhonic);
+                return selectedPhonic;
+            } 
+        }
+    }    
+
+    //About the Letters' Displayed:
+    function loadLetterChoices() {
+        
+        //Get the Letters choices of the current round
+        let datasetKey = "round"+round;
+        let dataSet = phonicData[datasetKey];
+        //Get the specific dataset (ex."round1" ->"randomSound" --> "choices")
+        let obtainChoices= Object.values(dataSet.choices);
+        console.log(obtainChoices);
+        
+        //Approach1
+        // // Get the individual letter elements
+        // const lettersElements = document.querySelectorAll('#letters-box .letters');
+        // console.log(lettersElements);
+        // // Using a for loop to fill in the data
+        // for (let i = 0; i < lettersElements.length; i++) {
+            //     lettersElements[i].innerText = obtainChoices[i];
+            //     // document.querySelector(lettersElements[i]).innerHTML = obtainChoices[i]; 
+            // }
+            
+            //Approach 2 (More Effecient)
+            
+            for (let i = 0; i <obtainChoices.length; i++) {
+                // Get the container element
+                let lettersBox = document.getElementById("letters-box");
+                
+                console.log("loadLetterChoices");
+            const divElement = document.createElement('div');
+
+            //Add class to the element to apply the css
+            divElement.classList.add('letters'); 
+
+            //Adding letter choice 
+            let content = document.createTextNode(obtainChoices[i]);
+            divElement.appendChild(content);
+
+            //Append the div to the "letters-box" container 
+            lettersBox.appendChild(divElement);
+        }
+
+        // // Call the callback function once the elements are loaded
+        // callback();
+    }
+
+    //Displaying the round number
+    function roundNumber() {
+    let roundNumber = document.querySelector("#roundNumber h3");
+
+    //Adding letter choice 
+    let currentRound = document.createTextNode("Round #" + round);
+    roundNumber.appendChild(currentRound);
+    }
+    
+    roundNumber();
+
+    //About User Input
+
+    // wait the letter boxes to be loaded 
+    loadLetterChoices();
+
+    //add event to check if the user clicked any button 
+    document.querySelectorAll('#letters-box .letters').forEach(function(letter){
+        console.log("event");
+        letter.addEventListener('click', checkAnswer)
+    });
+
+    function checkAnswer(element) {
+        //Get the content of the object clicked on
+        let clickedContent = element.target.textContent;
+        
+        console.log(clickedContent); 
+
+        let userSelect=userInput.textContent;
+
+        //Get the correct answer
+        let datasetKey = "round"+round;
+        let dataSet = phonicData[datasetKey];
+        let correctAnswer= Object.values(dataSet.randomSound[selectedPhonic[1]]); 
+        console.log(correctAnswer);
+    
+        if (userSelect == correctAnswer) {
+            scoreA++;
+        }
+
+        // Update currentPlayer
+        currentPlayer = (currentPlayer === "A") ? "B" : "A";
+
+        // Toggle the current player class
+        toggleCurrentPlayer(); //dynamically add/remove a class 
+    }
+    
+
+    
+
+
+    // Function to toggle the current player class
+    let playerADiv = document.querySelector("#playerA");
+    let playerBDiv = document.querySelector("#playerB");
+    
+    function toggleCurrentPlayer() {
+        playerADiv.classList.toggle("currentPlayer");
+        playerBDiv.classList.toggle("currentPlayer");
+    }
+
+    //About the Scores: 
+    // If the player selected the right letter option --> Get one point.
+    // Instantly increment and fill in the current score
+    // Store the previous round's score 
+    // Add logic to add the previous round's score
+    // After 6 sounds played, compare the scores and annouce the winner 
+
+    //About Game Reset 
+    //clear letters
+    //clear scores
+    //recall play function
+
+    //About Next Game
+    //clear letters
+    //clear scores
+    //call the next object in the data
+
+});
